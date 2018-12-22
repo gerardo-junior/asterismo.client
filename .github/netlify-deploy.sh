d78c04fe51bdf14fcb11112fe1df976788d60b9c
Gerardo Junior <me@gerardo-junior.com>
2018-12-22 14:49:02 -0300

    Back deploy with webhook

--- .github/netlify-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker-compose run npm run generate 
-netlify deploy -s $NETLIFY_SITE_ID -t $NETLIFY_ACCESS_TOKEN -d ./dist $([ "$TRAVIS_BRANCH" = "master" ] && echo "-p" || echo "-b $TRAVIS_BRANCH")" --message "travis build $TRAVIS_BRANCH"
