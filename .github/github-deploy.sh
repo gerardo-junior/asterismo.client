3f6ed34183ea043a28dd9bebd6210326fc2751c8
Gerardo Junior <me@gerardo-junior.com>
2018-12-22 05:22:44 -0300

    Adding travis ci and script deploy to github

---
+++ .github/github-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $HOME
+git clone --recurse-submodules --branch $TRAVIS_BRANCH https://github.com/gerardo-junior/asterismo.git
+cd asterismo
+git config user.email "bot@gerardo-junior.com"
+git config user.name "Bot of gerardo"
+git submodule update --remote client
+git add client
+git commit -m "TRAVIS client #${TRAVIS_BUILD_NUMBER} - Updating submodule client"
+git push --force --repo https://bot-of-gerardo:${GITHUB_BOT_AUTH_TOKEN}@github.com/gerardo-junior/asterismo
+cd ../ 
+rm -rf asterismo
