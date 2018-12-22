80ee229aa0b0cccc81a0e9202f674532dc0d9324
Gerardo Junior <me@gerardo-junior.com>
2018-12-22 18:01:17 -0300

    Adding branch to commit message

--- .github/github-deploy.sh
+++ .github/github-deploy.sh
@@ -7,7 +7,7 @@ git config user.email "bot@gerardo-junior.com"
 git config user.name "Bot of gerardo"
 git submodule update --remote client
 git add client
-git commit -m "TRAVIS asterismo client #${TRAVIS_BUILD_NUMBER} - Updating submodule client"
+git commit -m "TRAVIS asterismo client #${TRAVIS_BUILD_NUMBER} - Updating submodule client on ${TRAVIS_BRANCH} branch"
 git push --force --repo https://bot-of-gerardo:${GITHUB_BOT_AUTH_TOKEN}@github.com/gerardo-junior/asterismo
 cd ../ 
 rm -rf asterismo
