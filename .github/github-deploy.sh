1036c82613797b8b2bdb7bba0f85dd8bf1566a74
Gerardo Junior <me@gerardo-junior.com>
2018-12-23 16:53:22 -0300

    Adding checkout by hash in github deploy

--- .github/github-deploy.sh
+++ .github/github-deploy.sh
@@ -6,6 +6,9 @@ cd asterismo
 git config user.email "bot@gerardo-junior.com"
 git config user.name "Bot of Gerardo"
 git submodule update --remote client
+cd api
+git checkout $TRAVIS_COMMIT
+cd ../
 git add client
 git commit -m "TRAVIS #${TRAVIS_BUILD_NUMBER} - Updating submodule client on ${TRAVIS_BRANCH} branch"
 git push --force --repo https://bot-of-gerardo:${GITHUB_BOT_AUTH_TOKEN}@github.com/gerardo-junior/asterismo
